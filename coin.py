0de975a476b33f91261011c2de712226cb451071
adammachaj <37911187+adammachaj@users.noreply.github.com>
2019-03-28 13:12:35 +0100

    Update coin.py

--- coin.py
+++ coin.py
@@ -1,11 +1,11 @@
 class Coin(object):
     """Coins"""
 
-    coin_values = {"1 gr" : 0.01, "2 gr" : 0.02, "2 zl" : 2.0}
+    COINS = {"1 gr" : 0.01, "2 gr" : 0.02, "2 zl" : 2.0}
 
     def __init__(self, coin_id):
         self.coin_id = coin_id
-        self.value = coin_values[coin_id]
+        self.value = COINS[self.coin_id]
 
     def __str__(self):
         rep = str(self.value)
