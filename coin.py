50936fc350358dd734c60af6666a09042457bb08
adammachaj <37911187+adammachaj@users.noreply.github.com>
2019-03-28 13:17:14 +0100

    Update coin.py

--- coin.py
+++ coin.py
@@ -1,7 +1,9 @@
 class Coin(object):
     """Coins"""
 
-    COINS = {"1 gr" : 0.01, "2 gr" : 0.02, "2 zl" : 2.0}
+    COINS = {"1 gr" : 0.01, "2 gr" : 0.02, "5 gr" : 0.05,
+             "10 gr" : 0.1, "20 gr" : 0.2, "50 gr" : 0.5,
+             "1 zl" : 1.0, "2 zl" : 2.0, "5 zl" : 5.0}
 
     def __init__(self, coin_id):
         self.coin_id = coin_id
