a80105eb4ce5893b5a1089c8de43706cb75fd2ae
adammachaj <37911187+adammachaj@users.noreply.github.com>
2019-04-01 15:41:52 +0200

    Update collector.py

--- collector.py
+++ collector.py
@@ -13,11 +13,17 @@ class Collector(item.Item):
         rep = "Name: " + str(self.items[0]) + "\tCena: " + str(self.price)
         return rep
 
-    def add_item(self):
+    def add_item(self, i):
         if self.quantity < 11:
-            self.items.append(1)
+            self.items.append(i)
         else:
             print("Not enough slots in collector")
 
     def remove_items(self):
         self.items.clear()
+
+    def get_item(self):
+        if self.items[0] == None:
+            return "No sodas here"
+        else:
+            return self.items[0].name
