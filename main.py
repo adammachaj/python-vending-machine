2a2d5c4aa21f4739ed30d74a0a88aa2b29bd228d
adammachaj <37911187+adammachaj@users.noreply.github.com>
2019-04-01 15:54:21 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import machine, item, collector
+
+machine1 = machine.Machine(1)
+print(machine1.ITEMS[0])
+
+collector1 = collector.Collector(30)
+
+machine1.snacks[31] = item.Item("Pepsi")
+print(machine1.snacks.get(31))
+
+machine1.snacks[32] = item.Item("Cola")
+print(machine1.snacks.get(32))
+
+#collector1.add_item(machine.Machine.ITEMS[1])
+#print(collector1)
+
+print(machine1.snacks[31])
+
+print(machine1)
+
+machine1.snacks[32].add_item(item.Item("Cola"))
+print(machine1.snacks.get(32))
