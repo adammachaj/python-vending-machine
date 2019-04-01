5e115d7574f22ed90dd45a495f20d670de1fd245
adammachaj <37911187+adammachaj@users.noreply.github.com>
2019-04-01 15:41:35 +0200

    Update item.py

--- item.py
+++ item.py
@@ -1,15 +1,9 @@
 class Item(object):
     """Snack"""
 
-    code_number = 30
-
-    def __init__(self, id_snack, name = "<name>", price = 2.0, code = code_number):
-        self.id_snack = id_snack
+    def __init__(self, name = "<name>"):
         self.name = name
-        self.price = price
-        self.code = code
-        Item.code_number += 1
 
     def __str__(self):
-        rep = "ID: " + str(self.id_snack) + "\t" + self.name + "\tCena: " + str(self.price) + "\tKod: " + str(self.code)
+        rep = self.name
         return rep
